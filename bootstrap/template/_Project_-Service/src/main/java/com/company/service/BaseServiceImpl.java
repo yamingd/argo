8fa6b1f6ac04a11a4fd84d9ffb36f4114c671e8e
yamingd <yamingdeng@gmail.com>
2014-10-05 20:48:43 +0800

    account refactor

--- bootstrap/template/_Project_-Service/src/main/java/com/company/service/BaseServiceImpl.java
+++ bootstrap/template/_Project_-Service/src/main/java/com/company/service/BaseServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * Created by yaming_deng on 14-8-19.
  */
-public abstract class BaseServiceImpl<T> extends ServiceMSTemplate<T> {
+public abstract class BaseServiceImpl<T extends BaseEntity> extends ServiceMSTemplate<T> {
 
     @Autowired
     protected RedisBuket redisBuket;
