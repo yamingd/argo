6c062ca57a12533c81a7e9aeabd1c8b15bf72f4d
yaming_deng <yamingdeng@gmail.com>
2014-10-21 10:10:28 +0800

    bootstrap template

--- core/src/main/java/com/argo/core/web/MvcController.java
+++ core/src/main/java/com/argo/core/web/MvcController.java
@@ -3,11 +3,15 @@ package com.argo.core.web;
 import com.argo.core.base.BaseUser;
 import com.argo.core.exception.UserNotAuthorizationException;
 import com.argo.core.web.session.SessionUserHolder;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -36,5 +40,21 @@ public abstract class MvcController {
         logger.error(request.getPathInfo() + "?" + request.getQueryString());
         logger.error("@@@@Error.", ex);
         return "500";
+    }
+
+    protected void wrapError(BindingResult result, JsonResponse actResponse){
+        List<String> fields = Lists.newArrayList();
+        for(FieldError error : result.getFieldErrors()){
+            fields.add(error.getDefaultMessage());
+        }
+        actResponse.getData().add(fields);
+    }
+
+    protected void wrapError(BindingResult result, BsonResponse actResponse) throws Exception {
+        List<String> fields = Lists.newArrayList();
+        for(FieldError error : result.getFieldErrors()){
+            fields.add(error.getDefaultMessage());
+        }
+        actResponse.add(fields);
     }
 }
