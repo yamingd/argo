241c2e90408490206fcd43ff3fe0f94904f48d1c
yamingd <yamingdeng@gmail.com>
2014-12-22 21:52:41 +0800

    mvc

---
+++ mail/src/main/java/com/argo/mail/EmailTemplate.java
@@ -0,0 +1,19 @@
+package com.argo.mail;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yaming
+ * Date: 2014/12/22
+ * Time: 21:51
+ */
+public interface EmailTemplate {
+
+    /**
+     * 从模板生产内容.
+     * @return
+     */
+    String render(Map params);
+
+}
